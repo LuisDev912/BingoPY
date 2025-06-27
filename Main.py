9be22eac321f9d862e1204b9d238f84bb99a8a2f
Luis Calleja <luiscallejaenrique@gmail.com>
2025-06-27 16:05:08 -0300

    "logic and redability of the bingo improved - Added emoji to make it easier to read - Improved logic to avoid duplicate numbers in the carton - Fixed issue where matched numbers were not removed - Simplified output and made the game loop more interactive"

--- Main.py
+++ Main.py
@@ -2,25 +2,40 @@ from random import randint as rd
 import time as t
 
 repeatedNumbers = []
-carton = []
 
 def createPlayerCarton():
+    newCarton = []
+    
+    while len(newCarton) < 6:
+        num = rd(1, 100)
+        if num not in newCarton:
+            newCarton.append(num)
+    return newCarton
 
-    for i in range(6):
-        carton.append(rd(1, 100))
-    return f"Your carton is: {carton}"
+newCarton = createPlayerCarton()
 
 while len(repeatedNumbers) < 100:
     Rnumber = rd(1, 100)
+    print(newCarton)
+
     if Rnumber not in repeatedNumbers:
-        print(Rnumber)
+        print(f"☑️ {Rnumber}")
         t.sleep(0.75)  
         repeatedNumbers.append(Rnumber) 
-    else:
-        continue
-    
-    print(f"all the numbers are: {sorted(repeatedNumbers)}") 
-    if input("Do you want to continue? (yes/no): ").strip().lower() != "yes":
+
+        if Rnumber in newCarton:
+            print(f"💯You have a match!: {Rnumber} \n")
+            newCarton.remove(Rnumber)
+            t.sleep(0.90)
+
+    if len(newCarton) == 0:
+        print("You have completed your carton!💯")
+        break
+        
+
+
+    print(f"❗all the numbers are: {sorted(repeatedNumbers)}") 
+    if input("\n❓Do you want to continue? (yes/no): ").strip().lower() != "yes":
         break
     
 print("thanks for playing!")
